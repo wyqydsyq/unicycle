2ebe941e3a5f5d6d9ea677f728ff37098e551bbc
Damon Poole <damon@codafication.com.au>
2017-05-25 15:02:17 +1000

    Add comments to root webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,9 @@
-module.exports = [require('./config/webpack/server'), require('./config/webpack/client')]
+// this file collates multiple Webpack configs
+// so that we can build separate bundles with one call to webpack
+// don't actually specify configuration in here, either
+// edit the configs required in below or create & add a new one
+
+module.exports = [
+  require('./config/webpack/server'),
+  require('./config/webpack/client')
+]
