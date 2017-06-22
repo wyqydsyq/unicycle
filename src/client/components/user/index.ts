d0f8139c1733c400cf454357356d6515a404a78b
Damon Poole <damon@codafication.com.au>
2017-06-22 14:59:43 +1000

    Give user component defaults

--- src/client/components/user/index.ts
+++ src/client/components/user/index.ts
@@ -1,20 +1,39 @@
-import xs from 'xstream'
+import xs , { Stream } from 'xstream'
 import { form, input, button } from '@cycle/dom'
 
 // import { classes } from '../../styles'
 // import Styles from './styles'
 
-export const User = (sources) => ({
-  DOM: xs.of(
-    form('.setUser', [
-      input({
-        attrs: {
-          name: 'name'
-        }
-      }),
-      button(['Save'])
-    ])
+export interface UserData extends Stream<{}> {
+  id?: number
+  name?: string
+}
+
+export interface Sources {
+  user$?: UserData
+}
+
+export const User = (sources: Sources) => {
+  const values$ = (sources.user$ || xs.of({})).map(
+    (user: UserData) => ({
+      id: (user && user.id) ? user.id : 0,
+      name: (user && user.name) ? user.name : ''
+    })
   )
-})
+
+  return {
+    DOM: values$.map(user =>
+      form('.setUser', { key: user.id }, [
+        input({
+          attrs: {
+            name: 'name',
+            value: user.name
+          }
+        }),
+        button(['Save'])
+      ])
+    )
+  }
+}
 
 export default User
