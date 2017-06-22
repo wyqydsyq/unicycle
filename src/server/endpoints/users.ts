ea70dd4ddb782219497acd45fe5c07a441122aa6
Damon Poole <damon@codafication.com.au>
2017-06-22 14:59:02 +1000

    Add basic user API endpoint

--- src/server/endpoints/users.ts
+++ src/server/endpoints/users.ts
@@ -1,28 +1,75 @@
-// const getUser = (context, query = {}) => new Promise((res, rej) => {
-//   context._waterline
-//     .collections
-//     .users
-//     .find(query)
-//     .exec((error, result) => {
-//       if (error) {
-//         rej(error)
-//       } else {
-//         res(result)
-//       }
-//     })
-// })
+import * as R from 'ramda'
 
-export const Users = async userId => context => {
-  console.log(userId, context)
-  // switch (context.method) {
-  //   case 'GET':
-  //   default:
-  //     console.log(context.params)
-  //     break
-  // }
-  // // const users = await getUser(context)
-  // // console.log('CTX: ', users)
-  // context.body = 'Hello from the Koa API 🙂'
+export const Users = (userId?) => async context => {
+  let result
+  switch (context.method) {
+    default:
+    case 'GET':
+      result = await get(context, userId).catch(e => [])
+      break
+    case 'POST':
+      result = await set(context, { id: userId }, context.body)
+      break
+  }
+
+  context.body = result
 }
+
+const get = (context, query = {}) => new Promise((res, rej) => {
+  context._waterline
+    .collections
+    .users
+    .find(query)
+    .exec((error, result) => {
+      if (error) {
+        rej(error)
+      } else {
+        res(result)
+      }
+    })
+})
+
+const set = (context, query = {}, data = {}) => new Promise((res, rej) => {
+  // try to find the user
+  get(context, query).then(users => {
+    const user = R.head(users)
+
+    // if the user exists, update them
+    if (user) {
+      res(update(context, query, data))
+    }
+
+    // otherwise create them
+    res(create(context, data))
+  })
+})
+
+const update = (context, query = {}, data = {}) => new Promise((res, rej) => {
+  context._waterline
+    .collections
+    .users
+    .update(query, data)
+    .exec((error, result) => {
+      if (error) {
+        rej(error)
+      } else {
+        res(result)
+      }
+    })
+})
+
+const create = (context, data = {}) => new Promise((res, rej) => {
+  context._waterline
+    .collections
+    .users
+    .create(data)
+    .exec((error, result) => {
+      if (error) {
+        rej(error)
+      } else {
+        res(result)
+      }
+    })
+})
 
 export default Users
